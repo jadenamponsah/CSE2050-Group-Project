a3fefb5e09cf139478b2513310895a7476227cad
jadenamponsah <jamp4058@gmail.com>
2026-02-28 19:11:48 -0500

    Update GroupProject.py

--- GroupProject.py
+++ GroupProject.py
@@ -1,2 +1,21 @@
-print('Hello world')
-print('CSE 2050')
+class Course:
+    def __init__(self, course_credits, course_code, course_students):
+        self.course_credits = course_credits
+        self.course_code = course_code  
+        self.course_students = course_students
+
+    
+    def add_student(self, student):
+        self.course_students.append(student)
+
+
+    def get_students_count(self):
+        return len(self.course_students)
+    
+
+class Student:
+    def __init__(self, student_name, student_id):
+        self.student_name = student_name
+        self.student_id = student_id
+
+
